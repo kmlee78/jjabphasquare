ac4a364f766ad248a43ecbf1c6640277d0b2aaba
kmlee78 <jaoming787@gmail.com>
2021-10-23 19:08:23 +0900

    feat: add chart model

--- chart/models.py
+++ chart/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class ChartModel(models.Model):
+    name = models.CharField(max_length=20)
+    corp_code = models.CharField(max_length=6)
+
+    def __str__(self):
+        return self.name
