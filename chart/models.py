3e3a5a7b20520253367807690a2cdcf9c2cdf3c3
kmlee78 <jaoming787@gmail.com>
2021-10-25 19:16:16 +0900

    refactor: make main model in main directory

--- chart/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class ChartModel(models.Model):
-    corp_name = models.CharField(max_length=20)
-    stock_code = models.CharField(max_length=6)
-
-    def __str__(self):
-        return self.name
