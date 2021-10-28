05595d3d895d118d076fc31bedd5b3110ae1c643
kmlee78 <jaoming787@gmail.com>
2021-10-28 14:09:40 +0900

    feat: add community view functions

--- community/views.py
+++ community/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, "community/index.html")
+
+
+def post_detail(request):
+    pass
+
+
+def post_create(request):
+    pass
+
+
+def post_edit(request):
+    pass
+
+
+def post_delete(request):
+    pass
