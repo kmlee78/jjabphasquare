79dc9588fbab33811872fa3c65b847c2703bbb20
kmlee78 <jaoming787@gmail.com>
2021-11-18 19:41:21 +0900

    refactor: make main page one

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.start_page, name="start-page"),
-    path("main/", views.main_page, name="main-page"),
+    path("", views.start_page, name="main-page"),
 ]
--- main/views.py
+++ main/views.py
@@ -3,7 +3,3 @@ from django.shortcuts import render
 
 def start_page(request):
     return render(request, "main/title.html")
-
-
-def main_page(request):
-    return render(request, "main/index.html")
