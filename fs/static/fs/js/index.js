22932144cb6359572ceb384937a7b3ce8dfe631f
kmlee78 <jaoming787@gmail.com>
2021-11-28 15:08:19 +0900

    chore: change alert sign

--- fs/static/fs/js/index.js
+++ fs/static/fs/js/index.js
@@ -2,5 +2,5 @@ const fs_box = document.querySelector(".fs-box");
 const post_received = document.querySelector(".post-received");
 
 if (post_received != null && fs_box == null) {
-        alert("재무 정보를 찾을 수 없습니다. 종목 이름을 확인해주세요.");
+        alert("재무 정보를 찾을 수 없습니다. 종목 이름이나 공시 기간을 확인해주세요.");
 }
